a7d8cbcd73ebb34ee33f921f8ed835ab899a5539
unknown <mdebaprasad14@gmail.com>
2019-08-07 04:36:11 +0530

    Adding getMinTempSpread() and complted part one

--- weatherdata.py
+++ weatherdata.py
@@ -6,10 +6,23 @@ def getData(file):
 	headers = f.readline().split()    # get headers from file as list
 	return [dict(zip(headers, data.split())) for data in f.readlines()]  # return data as list of dictionary with headers as keys
 
+def getMinTempSpread(lst, func, minSpread=None):
+	'''Get min spread temp from lst by applying ftion 'f' '''
+	return (minSpread if lst == []
+			else( getMinTempSpread(lst[1:], func, lst[0]) if func(lst[0]) is not None
+				and (minSpread is None or func(lst[0]) < func(minSpread) )
+					else (getMinTempSpread(lst[1:], func, minSpread) )
+				)
+			)
+
 
 def weatherData():
-	file = 'weather.dat'  # file in which weather data present
+	'''lambda f is used to get temp spread for valid rows'''
+	file = 'weather.dat'  # file where weather data present
 	data = getData(file)
-	print(data)
+	day	 = getMinTempSpread(data, lambda temp: abs(float(temp['MxT'].replace('*','')) - float(temp['MnT'].replace('*','')))
+			if 'Dy' in temp else None)
+	print('The required day is - {}'.format(day['Dy']))
 
+# start of program
 weatherData()
