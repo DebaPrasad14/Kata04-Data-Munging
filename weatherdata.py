43577abc3d7caad34900d258903d0cf2a86896aa
unknown <mdebaprasad14@gmail.com>
2019-08-07 01:41:14 +0530

    Adding getData() & returning data as dict

--- weatherdata.py
+++ weatherdata.py
@@ -1,5 +1,12 @@
 # Part One: Weather Data
 
+def getData(file):
+	'''@file - input param "weather.dat" '''
+	f = open(file)		# open a file to read Data
+	headers = f.readline().split()    # get headers from file as list
+	return [dict(zip(headers, data.split())) for data in f.readlines()]  # return data as list of dictionary with headers as keys
+
+
 def weatherData():
 	file = 'weather.dat'  # file in which weather data present
 	data = getData(file)
