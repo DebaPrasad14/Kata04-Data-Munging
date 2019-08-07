c5786f97129fb4b24fbdf4bf21abae714f0d8ced
unknown <mdebaprasad14@gmail.com>
2019-08-08 01:39:37 +0530

    adding print st & removing useless chars

--- weather_solution_pandas.py
+++ weather_solution_pandas.py
@@ -1,9 +1,4 @@
 
-# coding: utf-8
-
-# In[69]:
-
-
 import pandas as pd
 from collections import OrderedDict
 
@@ -14,19 +9,14 @@ with open(file) as f:
     headers = f.readline().split()
     mydict = [OrderedDict(zip(headers,d.split())) for d in f.readlines()]
 
+
 df = pd.DataFrame(mydict[1:])   # creating a dataframe
 cols = ['MxT', 'MnT'] 
 for col in cols:
     df[col] = df[col].map(lambda x: str(x).lstrip('*').rstrip('*')).astype(float) # removing extra characters from value
 
+
 df['spread'] = df['MxT'] - df['MnT']    # finding diff between MxT & MnT = spread
-# mydict[1:]
 minv = df['spread'].min()    # getting minimum value in that spread column
-df
-
-
-# In[73]:
-
-
-df[['Dy']][df['spread']==minv]   # finding the day where spreaf = minv
+print(df[['Dy']][df['spread']==minv])   # finding the day where spreaf = minv
 
